68b598f222d619b813c3a079df7d4ff99d4bf102
Agustín Gimenez <geniwab@gmail.com>
2023-04-24 13:11:24 +0200

    Added missing file

---
+++ CoreSpectrum/Debug/Breakpoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreSpectrum.Debug
+{
+    public class Breakpoint
+    {
+        public string? Id { get; set; }
+        public object? Tag { get; set; }
+        public ushort Address { get; set; }
+        public bool Temporary { get; set; }
+        public bool Executed { get; set; }
+    }
+}
